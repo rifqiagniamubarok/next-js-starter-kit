c15f800792678c43093e21376353c29e1537841c
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-09-03 09:42:21 +0700

    feat: start

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,53 @@
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import axios from 'axios';
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'username' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        try {
+          const { data } = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+            username: credentials.username,
+            password: credentials.password,
+          });
+
+          const user = { id: data.token, name: data.username };
+          return user;
+        } catch (error) {
+          const errMsg = error.response.data.errors[0].message || 'Something problem, try again later!';
+          throw new Error(errMsg);
+        }
+      },
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      } else if (user) {
+        token.accessToken = user.token;
+      }
+
+      return token;
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    async session({ session, token }) {
+      return { ...session, token: token.sub };
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+export default NextAuth(authOptions);
